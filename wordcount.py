26c550bc3386e820422039996c7dcb813d7c0f2d
vagrant <vagrant@vagrant.vm>
2020-10-05 19:11:09 +0000

    first attempt at word counting function

--- wordcount.py
+++ wordcount.py
@@ -1,2 +1,7 @@
-# put your code here.
-print('hi')
+
+def counts_words(file):
+    open(file)      #opens file
+    word_count = {} #dictionary with words and their count
+    for letter in file:
+        word_count[letter] = word_count.get(letter, 0) + 1   
+    return word_count
