3827983bb7fa0d076992cc6d1217c78c00452b88
vagrant <vagrant@vagrant.vm>
2020-10-05 20:04:38 +0000

    made all text lowercase, removed trailing characters

--- wordcount.py
+++ wordcount.py
@@ -4,7 +4,12 @@ def counts_words(file):
     open_file = open(file)      #opens file
     word_count = {} #dictionary with words and their count
     for line in open_file:
-        list_words = line.split(' ')
+        line = line.lower()
+        str_lines = line.rstrip('\n')   #removes trailing \n
+        str_no_end_pun = str_lines.strip('.')
+        str_no_end_pun = str_lines.strip(',') #removes trailing .
+        str_no_end_pun = str_no_end_pun.rstrip('?') #removes trailing ?
+        list_words = str_no_end_pun.split(' ') #split words where there are spaces
         for word in list_words:
             word_count[word] = word_count.get(word, 0) + 1   
     return word_count
