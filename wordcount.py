429be9acff78707a77a7e8ece75ec9b01aa0dcd2
vagrant <vagrant@vagrant.vm>
2020-10-05 19:36:41 +0000

    function counts words but punctuation and \n are attached to word

--- wordcount.py
+++ wordcount.py
@@ -4,9 +4,10 @@ def counts_words(file):
     open_file = open(file)      #opens file
     word_count = {} #dictionary with words and their count
     for line in open_file:
-        for word in line:
+        list_words = line.split(' ')
+        for word in list_words:
             word_count[word] = word_count.get(word, 0) + 1   
-    print(word_count)
     return word_count
 
-counts_words('test.txt')
+word_count = counts_words('test.txt')
+print(word_count)
